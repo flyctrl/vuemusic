a27136e64495e2cb17e9979a4c59946fa8fda764
程宝生 <chenbaosheng@build4dream.com>
2019-07-22 13:58:22 +0800

    create vuemusic

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    singer
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'singer'
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -10,16 +9,27 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      redirect: '/recommend'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/rank',
+      name: 'Rank',
+      component: () => import(/* webpackChunkName: "rank" */ './components/rank/rank')
+    },
+    {
+      path: '/recommend',
+      name: 'Recommend',
+      component: () => import(/* webpackChunkName: "recommend" */ './components/recommend/recommend')
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: () => import(/* webpackChunkName: "search" */ './components/search/search')
+    },
+    {
+      path: '/singer',
+      name: 'Singer',
+      component: () => import(/* webpackChunkName: "about" */ './components/singer/singer')
     }
   ]
 })
