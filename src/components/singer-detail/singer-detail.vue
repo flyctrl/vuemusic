c37ef48c1fc1472ca6c5ee0be8c8453d7cc5acca
程宝生 <chenbaosheng@build4dream.com>
2019-07-30 16:30:23 +0800

    歌手详情数据处理

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -8,11 +8,13 @@
 import { mapGetters } from 'vuex'
 import { getSingerDetail } from 'api/singer'
 import { ERR_OK } from 'api/config'
+import { createSong } from 'common/js/song'
 
 export default {
   name: 'singerdetail',
   data () {
     return {
+      songs: []
     }
   },
   computed: {
@@ -33,8 +35,20 @@ export default {
       getSingerDetail(this.singer.id).then((res) => {
         if (res.code === ERR_OK) {
           console.log(res.data.list)
+          this.songs = this._normalizeSongs(res.data.list)
+          console.log(this.songs)
         }
       })
+    },
+    _normalizeSongs(list) {
+      let ret = []
+      list.forEach(item => {
+        let { musicData } = item
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
     }
   }
 }
